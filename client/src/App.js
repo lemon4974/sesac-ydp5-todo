41e782b6b921560764b6b3d0b1243ce4fdd94874
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-10-13 13:29:09 +0900

    feat: todo 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -32,12 +32,20 @@ function App() {
     //todoItems 배열에 newItems을 추가
     setTodoItems([...todoItems, newItem]);
   };
+
+  // todoItems 상태에 특정 투두를 삭제하는 일
+  const deleteItem = (targetItem) => {
+    //id값만 받아도 ok
+    console.log(targetItem);
+    const newTodoItems = todoItems.filter((item) => item.id !== targetItem.id);
+    setTodoItems(newTodoItems);
+  };
   return (
     <div className="App">
       <AddTodo addItem={addItem} />
       {/* todoItems 반복,  props 데이터(투두 객체)를 자식 컴포넌트에게 전달 */}
       {todoItems.map((item) => (
-        <Todo key={item.id} item={item} />
+        <Todo key={item.id} item={item} deleteItem={deleteItem} />
       ))}
     </div>
   );
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Todo({ item }) {
-  const { id, title, done } = item;
+export default function Todo({ item, deleteItem }) {
+  const [todoItem, setTodoItem] = useState(item);
+  const { id, title, done } = todoItem;
+
+  const onDeleteButtonClick = () => {
+    deleteItem(todoItem); //todoItem은 객체, id 값만 받아와도 되지만 추후 수정의 편의성 위해 값 다 받음
+  };
 
   return (
     <div>
@@ -12,6 +17,7 @@ export default function Todo({ item }) {
         defaultChecked={done}
       />
       <label htmlFor={`todo${id}`}>{title}</label>
+      <button onClick={onDeleteButtonClick}>delete</button>
     </div>
   );
 }
