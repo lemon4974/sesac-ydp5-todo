ac4ee97e29641a1ee0d7ea236c1ca1714f6b3a03
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-10-14 17:39:35 +0900

    study: crud 기능

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,20 @@ import AddTodo from './components/AddTodo';
 
 import axios from 'axios';
 import './styles/index.css';
-import Header from './components/Header';
 
 // axios를 이용해 백에서 데이터를 가져오기 때문에 axios import
 function App() {
-  console.log(process.env.REACT_APP_DB_HOST);
+  // console.log(process.env.REACT_APP_DB_HOST);
   const [todoItems, setTodoItems] = useState([]);
 
   useEffect(() => {
     const getTodos = async () => {
       const res = await axios.get(`${process.env.REACT_APP_DB_HOST}/todos`);
-      console.log(res.data);
+      // console.log('res 전체 >>', res);
+      // console.log(res.headers);
+      // console.log(res.data);
+      // console.log(res);
+      // console.log('request >>', res.request);
 
       setTodoItems(res.data);
     };
@@ -24,7 +27,7 @@ function App() {
 
   //todoItems 상태에 새로운 투두를 추가하는 일
   const addItem = async (newItem) => {
-    console.log(newItem); //{title: '저녁 먹기}
+    // console.log(newItem); //{title: '저녁 먹기}
 
     // //newItems id 키 값 넣고, neItem done 키 값
     // newItem.id = todoItems.length + 1;
@@ -37,6 +40,7 @@ function App() {
       newItem
       // request body에 추가되는 객체, newItem 이 이미 객체이기에 별도로 {newItem}으로 감싼 형태 불필요
     ); // axios.post('url' {})
+    console.log(res);
     // console.log(res.data);
     setTodoItems([...todoItems, res.data]); //나머지 정보 추가 후, data로 받아온 값 추가
   };
@@ -73,7 +77,7 @@ function App() {
       >
         {/* <Header /> */}
 
-        <div className="col-start-1 backdrop-blur-sm bg-white/30  w-3/4 h-2/5 py-6 mx-auto my-auto">
+        <div className="col-start-1 rounded-[0.5rem] backdrop-blur-sm bg-white/30  w-3/4 h-2/5 py-6 mx-auto my-auto">
           <AddTodo addItem={addItem} />
           {/* todoItems 반복,  props 데이터(투두 객체)를 자식 컴포넌트에게 전달 */}
           <div className=" w-2/4 h-2/5 py-6 mx-auto my-auto ">
